f78de9c14726a4c8895c8146f8ac6d303bdc8dac
Geuri Paulino <geruripaulino@gmail.com>
2020-11-20 20:34:08 -0400

    End of section 11

---
+++ client/src/app/core/test-error/test-error.component.ts
@@ -0,0 +1,50 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.scss']
+})
+export class TestErrorComponent implements OnInit {
+  baseUrl = environment.apiUrl;
+  validationErrors: any;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  get500Error(): void{
+    this.http.get(this.baseUrl + 'buggy/servererror').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.error(error);
+    });
+  }
+
+  get404Error(): void{
+    this.http.get(this.baseUrl + 'products/42').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.error(error);
+    });
+  }
+
+  get400Error(): void{
+    this.http.get(this.baseUrl + 'buggy/badrequest').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.error(error);
+    });
+  }
+
+  get400ValidationError(): void{
+    this.http.get(this.baseUrl + 'products/fortytwo').subscribe(response => {
+      console.log(response);
+    }, error => {
+      console.error(error);
+      this.validationErrors = error.errors;
+    });
+  }
+}
