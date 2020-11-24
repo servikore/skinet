4ab3a025e6944436eb538247c0f4bffb1453148c
Geuri Paulino <geruripaulino@gmail.com>
2020-11-24 12:32:10 -0400

    End of section 14

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,17 +6,45 @@ import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}},
-  {path: 'test-error', component: TestErrorComponent, data: {breadcrumb: 'Test Errors'}},
-  {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Error'}},
-  {path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not Found'}},
-  {path: 'shop', data: {breadcrumb: 'Shop'},
-    loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule) },
-  {path: '**', redirectTo: '', pathMatch: 'full'}
+  { path: '', component: HomeComponent, data: { breadcrumb: 'Home' } },
+  {
+    path: 'test-error',
+    component: TestErrorComponent,
+    data: { breadcrumb: 'Test Errors' },
+  },
+  {
+    path: 'server-error',
+    component: ServerErrorComponent,
+    data: { breadcrumb: 'Server Error' },
+  },
+  {
+    path: 'not-found',
+    component: NotFoundComponent,
+    data: { breadcrumb: 'Not Found' },
+  },
+  {
+    path: 'shop',
+    data: { breadcrumb: 'Shop' },
+    loadChildren: () =>
+      import('./shop/shop.module').then((mod) => mod.ShopModule),
+  },
+  {
+    path: 'basket',
+    data: { breadcrumb: 'Basket' },
+    loadChildren: () =>
+      import('./basket/basket.module').then((mod) => mod.BasketModule),
+  },
+  {
+    path: 'checkout',
+    data: { breadcrumb: 'Checkout' },
+    loadChildren: () =>
+      import('./checkout/checkout.module').then((mod) => mod.CheckoutModule),
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ client/src/app/basket/basket-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BasketComponent } from './basket.component';
+import { RouterModule } from '@angular/router';
+
+const routes = [
+  {path: '', component: BasketComponent}
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+export class BasketRoutingModule { }
